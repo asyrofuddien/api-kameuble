7ada2206ce1f2e02925a14e208695a50e833b709
zetta-muhammad-asyrofuddien <asyrofuddien11@gmail.com>
2024-10-05 17:21:41 +0700

    [INITIATION] Connect Database

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+require('dotenv').config();
+
 //import modul route
 const route = require('./routes/routes');
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
+
+// Connect database
+require('./database');
 
 route(app);
 
